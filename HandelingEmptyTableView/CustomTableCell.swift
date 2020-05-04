cae058ca8551a17dd70eaedc3087bf8759612fab
nikhilbalne <nikhil.b@ecoihub.com>
2020-05-04 19:16:33 +0530

    Added Code

---
+++ HandelingEmptyTableView/CustomTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableCell.swift
+//  HandelingEmptyTableView
+//
+//  Created by Nikhil Balne on 04/05/20.
+//  Copyright © 2020 Nikhil Balne. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
